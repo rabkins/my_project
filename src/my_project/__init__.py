d7e124d0a6e6d62076e708ebb01d2383bf104f5c
Marcin Raba <rabkins@Marcins-MacBook-Air.local>
2025-07-26 00:14:51 +0200

    my_project v0.0.1

--- src/my_project/__init__.py
+++ src/my_project/__init__.py
@@ -1,3 +1,7 @@
 # SPDX-FileCopyrightText: 2025-present Marcin r. Raba <rabkins@me.com>
 #
 # SPDX-License-Identifier: MIT
+
+from my_project import main
+
+__all__ = ["main"]
---
+++ src/my_project/main.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def main():
+    print("42")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
